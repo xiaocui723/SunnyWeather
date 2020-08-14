175bc84d8cbf2610f53bac6deed0733a2e90ec4f
xiaocui723 <xiaocui723@gmail.com>
2020-08-14 10:28:12 +0800

    搜索页面

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,17 @@
+package com.sunnyweather.android
+
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        const val TOKEN = "66JKoEnkpyikxzK5"
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
